ca05e241f81d49c9a8d8adcf36efd1b37cbec294
karlo <karlor041@gmail.com>
2018-08-03 06:53:58 +0300

    finished StationAccessor

--- src/org/greenblitz/gbEV3/common/StationAccessor.java
+++ src/org/greenblitz/gbEV3/common/StationAccessor.java
@@ -3,7 +3,6 @@ package org.greenblitz.gbEV3.common;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.channels.IllegalBlockingModeException;
@@ -15,70 +14,82 @@ import java.util.concurrent.locks.ReentrantLock;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 
-public final class StationAccessor {
+public final class StationAccessor extends Thread {
 
+	/**
+	 * The alliance and position of a robot in the arena
+	 * 
+	 * @author karlo
+	 */
 	public enum Alliance {
-		RED1(1), RED2(2), RED3(3),
-		BLUE1(1), BLUE2(2), BLUE3(3),
-		NONE(0);
-		
+		RED1(1), RED2(2), RED3(3), BLUE1(1), BLUE2(2), BLUE3(3), NONE(0);
+
 		public final int position;
-		
+
 		private Alliance(int position) {
 			this.position = position;
 		}
-		
+
 		public static boolean isRedAlliance(Alliance a) {
 			return a == RED1 || a == RED2 || a == RED3;
 		}
-		
+
 		public static boolean isBlueAlliance(Alliance a) {
 			return a == BLUE1 || a == BLUE2 || a == BLUE3;
 		}
 	}
 
+	/**
+	 * Type of the current played game; this may change during match
+	 * @author karlo
+	 */
 	public enum GameType {
 		TELEOP, AUTO, INVALID
 	}
 
+	/**
+	 * All data related to single joystick hid
+	 * @author karlo
+	 */
 	private static class NativeJoystickData {
 		public float[] axes;
 		public boolean[] povs;
 		public boolean[] buttons;
+		public boolean isValid;
 
-		public NativeJoystickData(int axesCount, int buttonCount, int povsCount) {
+		public NativeJoystickData(int axesCount, int buttonCount, int povsCount, boolean isValid) {
 			axes = new float[axesCount];
 			povs = new boolean[buttonCount];
 			buttons = new boolean[povsCount];
+			this.isValid = isValid;
 		}
 
 		public NativeJoystickData() {
-			this(6, 10, 8);
+			this(6, 10, 8, false);
 		}
 	}
-	
+
+	/**
+	 * Data unique to each match
+	 * 
+	 * @author karlo
+	 */
 	private static class MatchSpecificData {
 		public final String eventName;
 		public final String gameSpecificMessage;
 		public final int matchNumber;
-		public final GameType matchType;		
 		
-		public MatchSpecificData(String eventName, String gameSpecificMessage, int matchNumber, GameType matchType) {
+		public MatchSpecificData(String eventName, String gameSpecificMessage, int matchNumber) {
 			this.eventName = eventName;
 			this.gameSpecificMessage = gameSpecificMessage;
 			this.matchNumber = matchNumber;
-			this.matchType = matchType;		
-		}
-		
-		public MatchSpecificData(String gameSpecificMessage, int matchNumber, GameType matchType) {
-			this("Treasure Island", gameSpecificMessage, matchNumber, matchType);
-		}
-		
-		public MatchSpecificData(String gameSpecificMessage, int matchNumber) {
-			this(gameSpecificMessage, matchNumber, GameType.INVALID);
 		}
 	}
-
+	
+	/** 
+	 * Every piece of data available from the station
+	 * @author karlo
+	 */
 	private static class StationDataCache {
 		public NativeJoystickData[] joystickData;
 		public Alliance alliance;
@@ -100,29 +111,26 @@ public final class StationAccessor {
 	}
 
 	public static final int JOYSTICK_COUNT = 2;
-	
-	private static final long NEXT_JOYSTICK_UNPLUGGED_MESSAGE_TIME = 1000;
-	private static final String REQUIRED_JOYSTICK_UNPLUGGED_MESSAGE = "Joystick unplugged: ";
 
-	private static final int SOCKET_TIMEOUT = 500;	
-	
+	private static final long NEXT_JOYSTICK_UNPLUGGED_MESSAGE_INTERVAL = 1000;
+
+	private static final int SOCKET_TIMEOUT = 500;
+
 	private static final Gson GSON_OBJECT = new Gson();
-	private static final TypeAdapter<StationDataCache> JSON_CACHE_PARSER =
-			GSON_OBJECT.getAdapter(StationDataCache.class);
-	private static final TypeAdapter<MatchSpecificData> JSON_MATCH_DATA_PARSER = 
-			GSON_OBJECT.getAdapter(MatchSpecificData.class);
-	
-	
+	private static final TypeAdapter<StationDataCache> JSON_CACHE_PARSER = GSON_OBJECT
+			.getAdapter(StationDataCache.class);
+	private static final TypeAdapter<MatchSpecificData> JSON_MATCH_DATA_PARSER = GSON_OBJECT
+			.getAdapter(MatchSpecificData.class);
+
 	private static final StationAccessor instance = new StationAccessor();
-	
-	
+
 	private final ServerSocket SELF;
 	private final Socket STATION;
 	private final BufferedReader STATION_READER;
 	private final InputStreamReader STATION_INNER_READER;
-	private final PrintStream STATION_WRITER;
-	
-	private long m_lastJoystickUnpluggedMessageTime = -1;
+
+	private long m_nextJoystickUnpluggedMessageTime = -1;
+
 	private StationDataCache m_cache = new StationDataCache();
 	private final MatchSpecificData m_matchInfo;
 	private final Object m_cacheMutex = new Object();
@@ -130,6 +138,8 @@ public final class StationAccessor {
 	private final Lock m_waitForData = new ReentrantLock();
 	private final Condition m_hasDataArrived = m_waitForData.newCondition();
 	private int m_currentUpdateCount = 0;
+	
+	private volatile boolean m_keepThreadAlive = true;
 
 	private StationAccessor() {
 		try {
@@ -138,7 +148,6 @@ public final class StationAccessor {
 			STATION = SELF.accept();
 			STATION_INNER_READER = new InputStreamReader(STATION.getInputStream());
 			STATION_READER = new BufferedReader(STATION_INNER_READER);
-			STATION_WRITER = new PrintStream(STATION.getOutputStream());
 		} catch (IOException | SecurityException | IllegalBlockingModeException e) {
 			System.exit(9970);
 			throw new RuntimeException();
@@ -150,12 +159,19 @@ public final class StationAccessor {
 		return instance;
 	}
 
+	/** 
+	 * @return Is the robot in disabled mode
+	 */
 	public boolean isDisabled() {
 		synchronized (m_cacheMutex) {
 			return !m_cache.isEnabled;
 		}
 	}
 
+	/**
+	 * @return Is the robot in disabled mode
+	 * @retur
+	 */
 	public boolean isEnabled() {
 		synchronized (m_cacheMutex) {
 			return m_cache.isEnabled;
@@ -229,10 +245,80 @@ public final class StationAccessor {
 			return m_cache.alliance;
 		}
 	}
+
+	public float getJoystickAxis(int stick, int axis) {
+		if (stick < 0 || stick >= JOYSTICK_COUNT)
+			throw new IllegalArgumentException(
+					"Joystick index out of range: '" + stick + "', expected 0 - " + (JOYSTICK_COUNT - 1));
+
+		synchronized (m_cacheMutex) {
+			if (!m_cache.joystickData[stick].isValid) {
+				reportJoystickUnpluggedError(
+						"Invalid joystick input on port '" + stick + "', check if it is connected");
+				return 0;
+			} else {
+				return m_cache.joystickData[stick].axes[axis];
+			}
+		}
+	}
 	
+	public boolean getJoystickPov(int stick, int pov) {
+		if (stick < 0 || stick >= JOYSTICK_COUNT)
+			throw new IllegalArgumentException(
+					"Joystick index out of range: '" + stick + "', expected 0 - " + (JOYSTICK_COUNT - 1));
+
+		synchronized (m_cacheMutex) {
+			if (!m_cache.joystickData[stick].isValid) {
+				reportJoystickUnpluggedError(
+						"Invalid joystick input on port '" + stick + "', check if it is connected");
+				return false;
+			} else {
+				return m_cache.joystickData[stick].povs[pov];
+			}
+		}
+	}
+	
+	public boolean getJoystickButton(int stick, int button) {
+		if (stick < 0 || stick >= JOYSTICK_COUNT)
+			throw new IllegalArgumentException(
+					"Joystick index out of range: '" + stick + "', expected 0 - " + (JOYSTICK_COUNT - 1));
+
+		synchronized (m_cacheMutex) {
+			if (!m_cache.joystickData[stick].isValid) {
+				reportJoystickUnpluggedError(
+						"Invalid joystick input on port '" + stick + "', check if it is connected");
+				return false;
+			} else {
+				return m_cache.joystickData[stick].buttons[button];
+			}
+		}
+	}
+	
+	public int getMatchNumber() {
+		return m_matchInfo.matchNumber;
+	}
+	
+	public String getGameSpecificMessage() {
+		return m_matchInfo.gameSpecificMessage;
+	}
+	
+	public String getEventName() {
+		return m_matchInfo.eventName;
+	}
+
+	public void release() {
+		m_keepThreadAlive = false;
+	}
+	
+	@Override
+	public void run() {
+		while (m_keepThreadAlive)
+			aquireData();
+	}
+
 	private StationDataCache safeJsonAsCache() {
 		String jsonObj = safeGetString();
-		
+
 		try {
 			StationDataCache ret;
 			if ((ret = JSON_CACHE_PARSER.fromJson(jsonObj)) == null) {
@@ -248,10 +334,10 @@ public final class StationAccessor {
 			return null;
 		}
 	}
-	
+
 	private MatchSpecificData safeJsonAsMatchData() {
 		String jsonObj = safeGetString();
-		
+
 		try {
 			MatchSpecificData ret;
 			if ((ret = JSON_MATCH_DATA_PARSER.fromJson(jsonObj)) == null) {
@@ -267,7 +353,7 @@ public final class StationAccessor {
 			return null;
 		}
 	}
-	
+
 	private String safeGetString() {
 		try {
 			return STATION_READER.readLine();
@@ -277,12 +363,20 @@ public final class StationAccessor {
 			return "";
 		}
 	}
-	
+
 	private void logJsonParseError(String jsonObj) {
 		// TODO: implement this after merge
 	}
-	
+
 	private void logSocketError(IOException e) {
 		// TODO: implement this after merge
+	}
+
+	private void reportJoystickUnpluggedError(String message) {
+		long currentTime = RobotClock.currentTimeMicros();
+		if (currentTime > m_nextJoystickUnpluggedMessageTime) {
+			// TODO: implement this after merge
+			m_nextJoystickUnpluggedMessageTime = currentTime + NEXT_JOYSTICK_UNPLUGGED_MESSAGE_INTERVAL;
+		}
 	}
 }
